f489fce2d2fc680c8d7773d555797e719263c496
ykforerlang <1527997464@qq.com>
2019-06-02 18:43:02 +0800

    test(test): 提供抽象的getNewCode方法

--- test/help/utils.js
+++ test/help/utils.js
@@ -6,3 +6,22 @@
  *
  */
 
+import {parseCode, geneCode} from '../../src/util/uast'
+
+/**
+ * 通过trans把code转化为newCode
+ * @param code 输入代码
+ * @param info 转化参数
+ * @param trans 转化规则
+ * @returns {string} newCode
+ */
+export function getNewCode(code, info, ...trans) {
+
+    let ast = parseCode(code)
+    for(let i = 0; i < trans.length; i ++) {
+        const tranFunc = trans[i]
+        ast = tranFunc.call(null, ast, info)
+    }
+
+    return geneCode(ast)
+}
