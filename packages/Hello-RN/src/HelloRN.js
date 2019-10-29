1a370b210c1bcc111dd4f82994d75b4f7ae99eb2
ykforerlang <1527997464@qq.com>
2019-10-29 16:35:25 +0800

    refactor(@areslabs/hello-rn @areslabs/hello-wx): 丰富之

--- packages/Hello-RN/src/HelloRN.js
+++ packages/Hello-RN/src/HelloRN.js
@@ -5,24 +5,24 @@
  * LICENSE file in the root directory of this source tree.
  *
  */
- 
+
+
 import React from 'react'
 import {View, Text} from 'react-native'
 
-import {camelCase} from '@areslabs/stringutil-rn'
-
-export default class Hello extends React.Component {
+export default class HelloRN extends React.Component {
     render() {
         return (
             <View style={this.props.style}>
                 <Text
                     style={this.props.textStyle}
                     onPress={() => {
-                        console.log('Hello: ', this.props.name, ' !')
+                        console.log('HelloRN: ', this.props.name, ' !')
                         this.props.textPress && this.props.textPress()
                     }}
-                >Hello: {camelCase(this.props.name)}!</Text>
+                >HelloRN: {this.props.name}!</Text>
             </View>
         )
     }
 }
+
---
+++ packages/Hello-WX/miniprogram_dist/src/index.js
@@ -0,0 +1,6 @@
+import CompMySelf from "./index.comp"
+import { WxNormalComp } from "@areslabs/wx-react"
+const RNApp = {}
+
+Component(WxNormalComp(CompMySelf, RNApp))
+    
