156d8495bd084069888e1efa9a0f5d20fedde187
ykforerlang <1527997464@qq.com>
2019-11-26 18:06:03 +0800

    refactor(@areslabs/wx-router): 配合webpack打包机制

---
+++ packages/wx-router/rollup.config.js
@@ -0,0 +1,18 @@
+const babel = require('rollup-plugin-babel')
+
+export default {
+    input: './src/index.js',
+
+    output: [
+        { file: "dist/index.js", format: "cjs" },
+    ],
+
+    plugins: [
+        babel({
+            exclude: 'node_modules/**',
+            presets: ['@babel/preset-env'],
+        })
+    ],
+
+    external: ['@areslabs/wx-react']
+};
