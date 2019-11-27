101c9271fc3e3ccb02a3b1736db21e61353d3db4
ykforerlang <1527997464@qq.com>
2019-11-27 14:51:20 +0800

    fix(router): 添加类型定义

---
+++ packages/router/src/native/index.d.ts
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) Areslabs.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+export const Route: any
+export const Router: any
+export const TabRouter: any
+
+export const history: any
