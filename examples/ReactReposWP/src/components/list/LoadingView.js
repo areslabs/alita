9ef4d3c1ae08fec6a6b02dc010ac5b652c3cb8e7
ykforerlang <1527997464@qq.com>
2019-12-04 11:33:30 +0800

    refactor(alita): webpack 打包移除 维护目标小程序目录

--- examples/ReactReposWP/src/components/list/LoadingView.js
+++
@@ -1,6 +0,0 @@
-import CompMySelf from "./LoadingView.comp"
-import { WxNormalComp } from "@areslabs/wx-react"
-import RNApp from "../../../RNApp.js"
-
-Component(WxNormalComp(CompMySelf, RNApp))
-    
