5c4ac383629494b4cd8e26362de23c7a675cda9a
ykforerlang <1527997464@qq.com>
2019-12-10 15:34:09 +0800

    refactor(alita): app.js 静态

--- mptemp/app.js
+++ mptemp/app.js
@@ -1,8 +1,2 @@
-/**
- * Copyright (c) Areslabs.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- */
-
+require('./_rn_.js')
+App({})
