c5b98fb05082b70d908083f15b28ed6fdbeac7ba
ykforerlang <1527997464@qq.com>
2019-12-12 11:34:08 +0800

    chore(examples): v2.0 调整HelloWorldExpo

--- examples/HelloWorldExpo/babel.config.js
+++ examples/HelloWorldExpo/babel.config.js
@@ -1,6 +1,17 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-  };
+      presets: ['module:metro-react-native-babel-preset'],
+      plugins: [
+          [
+              'babel-plugin-module-resolver',
+              {
+                  alias: {
+                      'react-native-vector-icons': '@expo/vector-icons',
+                  },
+              },
+          ],
+          ['@babel/plugin-proposal-decorators', { legacy: true }],
+      ],
+  }
 };
