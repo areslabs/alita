5f6e6b700466959308ae1a5f7bc83c8e0b857096
ykforerlang <1527997464@qq.com>
2020-01-27 16:38:08 +0800

    refactor(wx-react-native): 减小ActivetyIndicator代码行

--- packages/wx-react-native/src/component/WXActivityIndicator/index.js
+++ packages/wx-react-native/src/component/WXActivityIndicator/index.js
@@ -18,6 +18,11 @@ export default class WXActivityIndicator extends RNBaseComponent{
     }
 }
 
+
+function getPartRect(color, opa, transform) {
+    return `<rect width="7" height="20" x="46.5" y="40" fill="${color}" style="opacity: ${opa}" rx="5" ry="5" transform="${transform}"/>`
+}
+
 function getLoadingSyle(props) {
     const {animating = true, color, hidesWhenStopped, size} = props
 
@@ -33,18 +38,18 @@ function getLoadingSyle(props) {
     var svgLoading = `
       <svg xmlns="http://www.w3.org/2000/svg" width="120" height="120" viewBox="0 0 100 100" version="1.1">
       <path fill="none" d="M0 0h100v100H0z"/>
-      <rect width="7" height="20" x="46.5" y="40" fill="${color}" style="opacity: 1" rx="5" ry="5" transform="translate(0 -30)"/>
-      <rect width="7" height="20" x="46.5" y="40" fill="${color}" style="opacity: 0.93" rx="5" ry="5" transform="rotate(30 105.98 65)"/>
-      <rect width="7" height="20" x="46.5" y="40" fill="${color}" style="opacity: 0.86" rx="5" ry="5" transform="rotate(60 75.98 65)"/>
-      <rect width="7" height="20" x="46.5" y="40" fill="${color}" style="opacity: 0.79" rx="5" ry="5" transform="rotate(90 65 65)"/>
-      <rect width="7" height="20" x="46.5" y="40" fill="${color}" style="opacity: 0.73" rx="5" ry="5" transform="rotate(120 58.66 65)"/>
-      <rect width="7" height="20" x="46.5" y="40" fill="${color}" style="opacity: 0.66" rx="5" ry="5" transform="rotate(150 54.02 65)"/>
-      <rect width="7" height="20" x="46.5" y="40" fill="${color}" style="opacity: 0.59" rx="5" ry="5" transform="rotate(180 50 65)"/>
-      <rect width="7" height="20" x="46.5" y="40" fill="${color}" style="opacity: 0.53" rx="5" ry="5" transform="rotate(-150 45.98 65)"/>
-      <rect width="7" height="20" x="46.5" y="40" fill="${color}" style="opacity: 0.46" rx="5" ry="5" transform="rotate(-120 41.34 65)"/>
-      <rect width="7" height="20" x="46.5" y="40" fill="${color}" style="opacity: 0.39" rx="5" ry="5" transform="rotate(-90 35 65)"/>
-      <rect width="7" height="20" x="46.5" y="40" fill="${color}" style="opacity: 0.33" rx="5" ry="5" transform="rotate(-60 24.02 65)"/>
-      <rect width="7" height="20" x="46.5" y="40" fill="${color}" style="opacity: 0.26" rx="5" ry="5" transform="rotate(-30 -5.98 65)"/>
+      ${getPartRect(color, 1, 'translate(0 -30)')}
+      ${getPartRect(color, 0.93, 'rotate(30 105.98 65)')}
+      ${getPartRect(color, 0.86, 'rotate(60 75.98 65)')}
+      ${getPartRect(color, 0.79, 'rotate(90 65 65)')}
+      ${getPartRect(color, 0.73, 'rotate(120 58.66 65)')}
+      ${getPartRect(color, 0.66, 'rotate(150 54.02 65)')}
+      ${getPartRect(color, 0.59, 'rotate(180 50 65)')}
+      ${getPartRect(color, 0.53, 'rotate(-150 45.98 65)')}
+      ${getPartRect(color, 0.46, 'rotate(-120 41.34 65)')}
+      ${getPartRect(color, 0.39, 'rotate(-90 35 65)')}
+      ${getPartRect(color, 0.33, 'rotate(-60 24.02 65)')}
+      ${getPartRect(color, 0.26, 'rotate(-30 -5.98 65)')}
   </svg>`
 
 
