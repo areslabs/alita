32d994230287b2b15e3fbb07b4aafce897b8cd2e
ykforerlang <1527997464@qq.com>
2020-04-16 20:36:12 +0800

    feat(alita-core): 增加对weixin目录app.json文件的处理
    affects: @areslabs/alita-core

--- packages/alita-core/src/extractWxCompFiles/extractEntryFile.ts
+++ packages/alita-core/src/extractWxCompFiles/extractEntryFile.ts
@@ -1,11 +1,25 @@
-import * as path from "path";
+import * as path from "path"
+import * as fse from "fs-extra"
 
 import configure from '../configure'
+
+const merge = require('deepmerge')
 
 export const handleChanged = (info) => {
 
     const newWxOutFiles = {}
-    const {appJSON} = info.entryInfo
+    let {appJSON} = info.entryInfo
+
+    // 增加对 weixin目录下的app.json 文件处理
+    const userAppJSONPath = path.resolve(configure.inputFullpath, 'weixin', 'app.json')
+    if (fse.existsSync(userAppJSONPath)) {
+        const userAppJSON = require(userAppJSONPath)
+        appJSON = merge(appJSON, userAppJSON)
+
+        // 移除输出目录的app.json
+        fse.unlinkSync(path.resolve(configure.outputFullpath, 'weixin', 'app.json'))
+    }
+
 
     const appJSONPath = path.resolve(configure.outputFullpath, 'app.json')
     newWxOutFiles[appJSONPath] = JSON.stringify(appJSON, null, '\t')
