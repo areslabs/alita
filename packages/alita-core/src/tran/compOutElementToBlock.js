ade3a5eaae9c69628149dc830bdb40917860d561
canfoo <178774618@qq.com>
2020-05-09 16:21:26 +0800

    fix(alita-core) 修复自定义组件最外层退化为block引起部分元素事件无效问题

--- packages/alita-core/src/tran/compOutElementToBlock.js
+++ packages/alita-core/src/tran/compOutElementToBlock.js
@@ -21,14 +21,29 @@ export default function compOutElementToBlock (ast, info) {
         exit: path => {
             if (path.type === 'JSXOpeningElement'
                 && path.node.name.name === 'view'
-                && getOriginal(path)  // 没有origial属性的view，可能是直接使用的小程序内置组件，这个时候view可能有其他属性，故无法转化为block
                 && isRenderReturn(path)
 				&& !getAttri(path, 'onLayout') // 有onLayout属性的，无法转化为block
             ) {
-                path.node.name.name = 'block'
+                const original = getOriginal(path)
+                /*
+                    没有origial属性的view，可能是直接使用的小程序内置组件，这个时候view可能有其他属性，故无法转化为block
+                    同时也需要满足origial不是为TouchableWithoutFeedback、TouchableOpacity、TouchableHighlight、
+                    AnimatedView、AnimatedImage和AnimatedText，因为origial为这些内容的话，可能需要在view元素绑定一
+                    些事件（比如点击事件），如果退化为block，则事件就失效了
+                */
+                if (original
+                    && original !== 'TouchableWithoutFeedback'
+                    && original !== 'TouchableOpacity'
+                    && original !== 'TouchableHighlight'
+                    && original !== 'AnimatedView'
+                    && original !== 'AnimatedImage'
+                    && original !== 'AnimatedText'
+                ) {
+                    path.node.name.name = 'block'
 
-                if (path.parentPath.node.closingElement) {
-                    path.parentPath.node.closingElement.name.name = 'block'
+                    if (path.parentPath.node.closingElement) {
+                        path.parentPath.node.closingElement.name.name = 'block'
+                    }
                 }
             }
 
